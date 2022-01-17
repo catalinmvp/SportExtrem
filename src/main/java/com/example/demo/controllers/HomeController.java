7813fa675b58d839664c9b659c72ab0d687f82ee
Andrei Catalin <nichitaandrei21@yahoo.com>
2022-01-17 22:16:24 +0200

    Final product xd

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @RequestMapping("/")
 public class HomeController {
+
     @GetMapping
     public String home(){
         return "welcome";
