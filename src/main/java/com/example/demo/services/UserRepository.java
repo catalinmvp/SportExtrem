a1feedc739188ef87254306496f1d9ab708fcc36
catalinmvp <nichitaandrei21@yahoo.com>
2022-03-22 16:18:32 +0200

    first and final commit

--- src/main/java/com/example/demo/services/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.services;
-
-import com.example.demo.domain.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findUsersByUsername(String username);
-}
--- src/main/java/com/example/demo/services/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.services;
-
-import com.example.demo.domain.User;
-
-public interface UserService {
-    User save(User user);
-}
